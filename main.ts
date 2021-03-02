269e3deb48af7a6b13def9c2d98556186a6d2613
J. Enrique Agudo <jeagudo@unex.es>
2021-03-02 16:53:15 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,8 @@ input.onButtonPressed(Button.A, function () {
     bits = bits - 1
     Numero = Numero + 2 ** bits
     if (bits == 0) {
-        basic.showString(convertToText(Numero))
+        basic.showString("" + (Numero))
+        radio.sendNumber(Numero)
         bits = NumeroBits
         Numero = 0
     }
@@ -13,6 +14,7 @@ input.onButtonPressed(Button.B, function () {
     bits = bits - 1
     if (bits == 0) {
         basic.showNumber(Numero)
+        radio.sendNumber(Numero)
         bits = NumeroBits
         Numero = 0
     }
@@ -20,16 +22,11 @@ input.onButtonPressed(Button.B, function () {
 let Numero = 0
 let bits = 0
 let NumeroBits = 0
+radio.setGroup(1)
 NumeroBits = 7
 bits = NumeroBits
 // Número decimal a calcular
 Numero = 0
 basic.forever(function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+	
 })
